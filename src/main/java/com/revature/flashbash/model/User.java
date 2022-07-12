966a59ce6fbf929226540cd9b872da4c68869f83
bpinkerton <brandon.a.pinkerton@gmail.com>
2022-07-11 19:13:35 -0500

    update User model to use lombok annotations

--- src/main/java/com/revature/flashbash/model/User.java
+++ src/main/java/com/revature/flashbash/model/User.java
@@ -1,8 +1,13 @@
 package com.revature.flashbash.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 
 @Entity(name = "users")
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class User { // the default entity name would have been user
 
     @Id
@@ -19,64 +24,4 @@ public class User { // the default entity name would have been user
 
     @Enumerated
     private UserType userType;
-
-
-
-
-
-
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public User setUserId(Integer userId) {
-        this.userId = userId;
-        return this;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public User setUsername(String username) {
-        this.username = username;
-        return this;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public User setPassword(String password) {
-        this.password = password;
-        return this;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public User setFirstName(String firstName) {
-        this.firstName = firstName;
-        return this;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public User setLastName(String lastName) {
-        this.lastName = lastName;
-        return this;
-    }
-
-    public UserType getUserType() {
-        return userType;
-    }
-
-    public User setUserType(UserType userType) {
-        this.userType = userType;
-        return this;
-    }
 }
